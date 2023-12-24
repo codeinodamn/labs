b043db0c9300e114fe7be26b6c939391460c244f
codeinodamn <145688106+codeinodamn@users.noreply.github.com>
2023-12-24 18:07:13 +0700

    Add files via upload

---
+++ lab8/registracia.php
@@ -0,0 +1,62 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+        }
+
+        form {
+            max-width: 400px;
+            margin: 50px auto;
+            padding: 20px;
+            background: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        input[type="text"],
+        input[type="password"],
+        input[type="date"] {
+            width: 100%;
+            padding: 10px;
+            margin: 10px 0;
+            box-sizing: border-box;
+        }
+
+        input[type="submit"] {
+            background: #4caf50;
+            color: #fff;
+            border: none;
+            padding: 10px;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background: #45a049;
+        }
+    </style>
+</head>
+<body>
+    <form action="process.php" method="post">
+        <h2>Регистрация</h2>
+        <input type="text" name="full_name" placeholder="ФИО" required>
+        <input type="text" name="login" placeholder="Логин" required>
+        <input type="password" name="password" placeholder="Пароль" required>
+        <input type="date" name="birthdate" required>
+        <input type="submit" value="Зарегистрироваться">
+    </form>
+</body>
+</html>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $full_name = $_POST["full_name"];
+    $login = $_POST["login"];
+    $password = $_POST["password"];
+    $birthdate = $_POST["birthdate"];
+
+    
+}
+?>
